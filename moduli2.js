d7abc9b461f4a7e2306bbe7a083a4381df8b7b52
Andrian Gaidarji <traian-007@mail.ru>
2021-10-13 17:00:30 +0300

    homework2

--- moduli2.js
+++ moduli2.js
@@ -1,47 +1,13 @@
-import  alert  from "./moduli1.js"
-
 const $btn2 = document.getElementById('btn2')
-$btn2.addEventListener('click', alert)
-////////////////////////////////////////////////////////////////
-class Animal {
-    #name;
-    constructor(name) {
-        this.#name = name
-    }
-}
+$btn2.addEventListener("click", e => {
+    e.preventDefault();
 
-class Dog extends Animal {
-    constructor(name) {
-        super(name) 
-        this.name = name
-    }
-    // created(){return new Date()}
-}
-const ab = new Dog('bob')
-ab.created = new Date()
-// console.log(ab.created())
-console.log(ab.created)
-console.log(ab.name)
-
-////////////////////////////////////////////////
-const $btn1 = document.getElementById('btn1')
-
-async function fetchJson(url) {
-    try {
-        let request = await fetch(url);
-        let text = await request.text();
-        return JSON.parse(text);
-    }catch (err) {
-        console.log(`Error: ${err.stack}`);
-    }
-}
-
-async function getResp() {
-    let resp = await fetchJson('https://swapi.dev/api/people/')
-    console.log(resp)
-}
-
-$btn1.addEventListener('click', getResp )
-
-//////////////////////////////////////////////////////////
-
+    import('/moduli1.js')
+      .then(module => {
+        module.loadPageInto($btn2);
+      })
+      .catch(err => {
+        $btn2.textContent = err.message;
+      });
+    alert() 
+});
