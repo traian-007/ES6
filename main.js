ec9c2539ea8be12847b3c867f211ca817a5b9c09
Andrian Gaidarji <traian-007@mail.ru>
2021-10-18 13:52:21 +0300

    getSuper

--- main.js
+++ main.js
@@ -1,35 +1,21 @@
 class Animal {
     #name;
-    constructor() {
-        this.#name = "name"
-    }
-    get boom() {
-        return this.#name
-    }
-    set changeName(name) {
+    constructor(name) {
         this.#name = name
+    }
+    get name() {
+        return this.#name
     }
 }
 
 class Dog extends Animal {
     constructor(name) {
-        super()
-        this.name = name
-    }
-    parentName() {
-            console.log(this.boom)
-        }
-    changeParentName(name) {
-        this.changeName = name
-    }
-    changeParentNameViaDogName() {
-        this.changeName = this.name
+        super(name)
+        this.created = new Date()
     }
 }
-const ab = new Dog('bob')
-ab.created = new Date()
+
+const ab = new Dog('cahk')
 console.log(ab.created)
 console.log(ab.name)
-console.log(ab.parentName())
-ab.changeParentName("gicu")
-console.log(ab.parentName())
+
